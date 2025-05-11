40640a5e98690ebd56cd169c5b003328088954eb
David Goldstein <93741283+davidvfx07@users.noreply.github.com>
2025-05-11 15:58:29 -0700

    Updated.

--- files/finish-setup.sh
+++ files/finish-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo Recieved home dir: $1
-
-cp -rf /usr/etc/finish-setup/config/* $1/.config
+cp -rf /usr/etc/finish-setup/config/* ~/.config
 
 bash <(curl -s https://raw.githubusercontent.com/blue-build/cli/main/install.sh)
