5b6d131a56d962a3f64e92ff81e42f8b314f92c1
David Goldstein <93741283+davidvfx07@users.noreply.github.com>
2025-05-11 19:39:40 -0700

    Updated.

--- files/finish-setup.sh
+++ files/finish-setup.sh
@@ -9,3 +9,7 @@ cp -rf /usr/etc/finish-setup/config/* /home/$ACTIVE_USER/.config
 chown -R $ACTIVE_USER:$ACTIVE_USER ~/.config/
 
 bash <(curl -s https://raw.githubusercontent.com/blue-build/cli/main/install.sh)
+
+su $ACTIVE_USER -c "curl https://nim-lang.org/choosenim/init.sh -sSf | sh"
+
+echo export PATH=/var/home/david/.nimble/bin:$PATH >> /home/$ACTIVE_USER/.profile
