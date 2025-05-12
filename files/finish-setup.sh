702e184091743c65400f5a8f307a004e84f2bff5
David Goldstein <93741283+davidvfx07@users.noreply.github.com>
2025-05-12 14:02:09 -0700

    Updated.

--- files/finish-setup.sh
+++ files/finish-setup.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 ACTIVE_USER=$(loginctl list-users --no-legend | cut -d' ' -f2 -z | tr -d '\000')
+HOME=/home/$ACTIVE_USER
 
 echo Active user: $ACTIVE_USER
 
-cp -rf /usr/etc/finish-setup/config/* /home/$ACTIVE_USER/.config
+cp -r /usr/etc/finish-setup/config/* $HOME/.config
+chown -R $ACTIVE_USER:$ACTIVE_USER $HOME/.config/
 
-chown -R $ACTIVE_USER:$ACTIVE_USER ~/.config/
-
-bash <(curl -s https://raw.githubusercontent.com/blue-build/cli/main/install.sh)
+if [ -f "/bin/bluebuild" ]; then
+  echo "bluebuild exists, not installing."
+else
+  bash <(curl -s https://raw.githubusercontent.com/blue-build/cli/main/install.sh)
+fi
 
 su $ACTIVE_USER -c "curl https://nim-lang.org/choosenim/init.sh -sSf | sh"
 
-echo export PATH=/var/home/david/.nimble/bin:$PATH >> /home/$ACTIVE_USER/.profile
+rpm-ostree kargs --delete rhgb
+rpm-ostree kargs --delete quiet
+rpm-ostree kargs --append-if-missing nvidia_drm.modeset=1 --append-if-missing nvidia_drm.fbdev=1 
+
+mkdir $HOME/.local/bin
+ln -s /var/lib/flatpak/exports/bin/org.mozilla.firefox $HOME/.local/bin/firefox
+ln -S /var/lib/flatpak/exports/bin/org.chromium.Chromium $HOME/.local/bin/chromium
+ln -s /var/lib/flatpak/exports/bin/com.usebottles.bottles $HOME/.local/bin/bottles
