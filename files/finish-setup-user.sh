b0d6ede592d96b0544a8fa88b095e750feb0b28d
David Goldstein <93741283+davidvfx07@users.noreply.github.com>
2025-05-18 19:29:14 -0700

    Implement uwsm.

--- files/finish-setup-user.sh
+++ files/finish-setup-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -n -r /usr/etc/finish-setup/config/* ~/.config || true
+cp -n -r /usr/etc/finish-setup/home/* ~/ || true
 
 #curl https://nim-lang.org/choosenim/init.sh -sSf | sh
 
