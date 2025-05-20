86427970b38fbb6166f9ab7c9b393bb975de297c
David Goldstein <93741283+davidvfx07@users.noreply.github.com>
2025-05-20 16:28:07 -0700

    Updated with drag-n-drop and color management supoprt.

--- files/finish-setup-user.sh
+++ files/finish-setup-user.sh
@@ -3,6 +3,7 @@
 cp -n -r /usr/etc/finish-setup/home/* ~/ || true
 
 #curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+cargo install ripdrag
 
 mkdir ~/.local/bin || true
 ln -s /var/lib/flatpak/exports/bin/org.mozilla.firefox ~/.local/bin/firefox || true
